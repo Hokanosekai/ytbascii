b0f38f7c9e2df50b70c89ef514d4ec1372ea6315
Hokanosekai <69720899+Hokanosekai@users.noreply.github.com>
2023-07-13 15:59:24 +0200

    Fetch information

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,56 +1,23 @@
-extern crate hyper;
-extern crate hyper_rustls;
-extern crate google_youtube3 as youtube3;
+extern crate reqwest;
+extern crate tokio;
+extern crate serde;
 
-use youtube3::{Result, Error};
-use std::default::Default;
-use youtube3::{YouTube, oauth2, hyper, hyper_rustls, chrono, FieldMask};
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 
-// Get an ApplicationSecret instance by some means. It contains the `client_id` and 
-// `client_secret`, among other things.
-const secret: oauth2::ApplicationSecret = Default::default();
-// Instantiate the authenticator. It will choose a suitable authentication flow for you, 
-// unless you replace  `None` with the desired Flow.
-// Provide your own `AuthenticatorDelegate` to adjust the way it operates and get feedback about 
-// what's going on. You probably want to bring in your own `TokenStorage` to persist tokens and
-// retrieve them from storage.
-const AUTH = oauth2::InstalledFlowAuthenticator::builder(
-        secret,
-        oauth2::InstalledFlowReturnMethod::HTTPRedirect,
-    ).build().await.unwrap();
-let mut hub = YouTube::new!(hyper::Client::builder().build(hyper_rustls::HttpsConnectorBuilder::new().with_native_roots().https_or_http().enable_http1().enable_http2().build()), auth);
-// You can configure optional parameters by calling the respective setters at will, and
-// execute the final call using `doit()`.
-// Values shown here are possibly random and not representative !
-let result = hub.videos().list(&vec!["et".into()])
-             .video_category_id("magna")
-             .region_code("no")
-             .page_token("ipsum")
-             .on_behalf_of_content_owner("voluptua.")
-             .my_rating("At")
-             .max_width(-8)
-             .max_results(21)
-             .max_height(-2)
-             .locale("takimata")
-             .add_id("amet.")
-             .hl("duo")
-             .chart("ipsum")
-             .doit().await;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url = "https://www.googleapis.com/youtube/v3/videos?id=aPkSvjlzLx0&part=snippet,statistics,fileDetails&key=AIzaSyDHTKjtUchUxUOzCtYW4V_h1zzcyd0P6c0";
 
-match result {
-    Err(e) => match e {
-        // The Error enum provides details about what exactly happened.
-        // You can also just use its `Debug`, `Display` or `Error` traits
-         Error::HttpError(_)
-        |Error::Io(_)
-        |Error::MissingAPIKey
-        |Error::MissingToken(_)
-        |Error::Cancelled
-        |Error::UploadSizeLimitExceeded(_, _)
-        |Error::Failure(_)
-        |Error::BadRequest(_)
-        |Error::FieldClash(_)
-        |Error::JsonDecodeError(_, _) => println!("{}", e),
-    },
-    Ok(res) => println!("Success: {:?}", res),
+    let response = reqwest::get(url)
+        .await;
+
+    let json = response.unwrap().text().await.unwrap();
+
+    let data: serde_json::Value = serde_json::from_str(&json)?;
+
+    println!("{}", data["videoDetails"]);
+    println!("{:#?}", data);
+
+    Ok(())
 }
